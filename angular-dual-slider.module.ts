5239dfdeca32d730517f81803cbe9ce2391051cb
Jurshsmith <jurshsmith@gmail.com>
2019-04-24 17:47:22 +0100

    added parts

--- angular-dual-slider.module.ts
+++ angular-dual-slider.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DualSliderComponent } from './dual-slider/dual-slider.component';
+import {MatButtonModule, MatCheckboxModule, MatGridListModule} from '@angular/material';
 
 @NgModule({
-  declarations: [],
+  declarations: [DualSliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
+  ],
+  exports: [
+    DualSliderComponent
   ]
 })
 export class AngularDualSliderModule { }
